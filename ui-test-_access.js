3f3899ab4789faf52b9ccb96ef96e898805288f5
Rosano Coutinho <pub@xeari.com>
2022-01-18 12:01:12 -0500

    Lint

--- ui-test-_access.js
+++ ui-test-_access.js
@@ -11,7 +11,7 @@ Object.entries({
 
 	ROCOBulletinSubmitButton: '.ROCOBulletinSubmitButton',
 }).map(function (e) {
-	return global[e.shift()]  = e.pop();
+	return global[e.shift()] = e.pop();
 });
 
 describe('ROCOBulletin_Access', function () {
