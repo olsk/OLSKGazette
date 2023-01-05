5f5db5a31aa109f0985a846ec78ebf4c78bb3512
Rosano Coutinho <pub@xeari.com>
2023-01-05 10:46:31 +0100

    Translate de

--- controller.js
+++ controller.js
@@ -8,7 +8,7 @@ const mod = {
 			OLSKRouteFunction (req, res, next) {
 				return res.OLSKExpressLayoutRender(require('path').join(__dirname, 'main'), Object.fromEntries((new URLSearchParams(req.query)).entries()));
 			},
-			OLSKRouteLanguageCodes: ['en', 'fr', 'es', 'pt'],
+			OLSKRouteLanguageCodes: ['en', 'fr', 'es', 'pt', 'de'],
 		}];
 	},
 
