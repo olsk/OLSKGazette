4a1f8050032b796125d1ae33ef0cfc5df5491bff
Rosano Coutinho <pub@xeari.com>
2023-12-02 16:16:07 -0300

    Use Ghost embed form

--- ui-test-_access.js
+++ ui-test-_access.js
@@ -2,14 +2,6 @@ const kDefaultRoute = require('./controller.js').OLSKControllerRoutes().shift();
 
 Object.entries({
 	ROCOBulletin: '.ROCOBulletin',
-
-	ROCOBulletin: '.ROCOBulletin',
-	ROCOBulletinAddressField: '.ROCOBulletinAddressField',
-	ROCOBulletinProjectField: '.ROCOBulletinProjectField',
-	ROCOBulletinProjectFieldButtondown: '.ROCOBulletinProjectFieldButtondown',
-	ROCOBulletinSubmitButton: '.ROCOBulletinSubmitButton',
-
-	ROCOBulletinFeedback: '.ROCOBulletinFeedback',
 }).map(function (e) {
 	return global[e.shift()] = e.pop();
 });
@@ -22,30 +14,6 @@ describe('ROCOBulletin_Access', function () {
 
 	it('shows ROCOBulletin', function () {
 		return browser.assert.elements(ROCOBulletin, 1);
-	});
-
-	it('shows ROCOBulletin', function () {
-		return browser.assert.elements(ROCOBulletin, 1);
-	});
-
-	it('shows ROCOBulletinAddressField', function () {
-		return browser.assert.elements(ROCOBulletinAddressField, 1);
-	});
-
-	it('shows ROCOBulletinProjectField', function () {
-		return browser.assert.elements(ROCOBulletinProjectField, 1);
-	});
-
-	it('shows ROCOBulletinProjectFieldButtondown', function () {
-		return browser.assert.elements(ROCOBulletinProjectFieldButtondown, 1);
-	});
-
-	it('shows ROCOBulletinSubmitButton', function () {
-		return browser.assert.elements(ROCOBulletinSubmitButton, 1);
-	});
-
-	it('shows ROCOBulletinFeedback', function () {
-		return browser.assert.elements(ROCOBulletinFeedback, 1);
 	});
 
 });
